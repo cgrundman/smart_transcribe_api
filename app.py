0ce01e4b6766fcf9e90403ad147e53811fe3635d
cgrundman <92883095+cgrundman@users.noreply.github.com>
2023-12-17 01:19:49 +0100

    implemented whisper_ai functionality

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
-# import json
 from flask import Flask, jsonify  # , request, render_template, redirect
-import whisper_ai
-import whisper_timestamped as whisper
-import os
-from scipy.io import wavfile
+from whisper_ai import TranscribeAudio
 
-# app = Flask(__name__)
-#
-# employees = [{'id': 1, 'name': 'Ashley'},
-#              {'id': 2, 'name': 'Kate'},
-#              {'id': 3, 'name': 'Joe'}]
-#
-# things = [{'id': 1, 'name': 'Banana'},
-#           {'id': 2, 'name': 'Apple'},
-#           {'id': 3, 'name': 'Pear'}]
-#
-#
-# @app.route('/employees', methods=['GET'])
-# def get_employees():
-#     return jsonify(employees)
-#
-#
-# @app.route('/ai', methods=['GET'])
-# def ai_output():
-#     return "Hello, I am your AI."  # TODO (simple example from ai model)
-#
-#
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
 
-# help(whisper.load_audio)
-samplerate, data = wavfile.read("./audio/words.wav")
+app = Flask(__name__)
 
-# whisper_ai.ai_output()
+employees = [{'id': 1, 'name': 'Ashley'},
+             {'id': 2, 'name': 'Kate'},
+             {'id': 3, 'name': 'Joe'}]
+
+things = [{'id': 1, 'name': 'Banana'},
+          {'id': 2, 'name': 'Apple'},
+          {'id': 3, 'name': 'Pear'}]
+
+
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    return jsonify(employees)
+
+
+@app.route('/whisper_ai', methods=['GET'])
+def whisper_ai():
+    transcription = TranscribeAudio().output()['text']
+    return transcription
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- whisper_ai.py
+++ whisper_ai.py
@@ -1,15 +1,19 @@
 import whisper_timestamped as whisper
-import json
-import os
 
 
-def ai_output():
-    audio = whisper.load_audio("./audio/words.wav")
+class TranscribeAudio:
+    def __init__(self):
+        # Test Audio Files
+        # audio, language = whisper.load_audio("./audio/words.wav"), "fr"
+        self.audio, self.language = whisper.load_audio("./audio/gloria.mp3"), "en"
 
-    model = whisper.load_model("tiny", device="cpu")
+    def output(self):
+        # Test Audio Files
+        # audio, language = whisper.load_audio("./audio/words.wav"), "fr"
+        # audio, language = whisper.load_audio("./audio/gloria.mp3"), "en"
 
-    result = whisper.transcribe(model, audio, language="fr")
+        model = whisper.load_model("tiny", device="cpu")
 
-    return print(json.dumps(result, indent=2, ensure_ascii=False))
+        result = whisper.transcribe(model, self.audio, language=self.language)
 
-# help(whisper.load_audio)
+        return result

732009986cb9566960e754d21a1ac4ababd6ec32
cgrundman <92883095+cgrundman@users.noreply.github.com>
2023-12-18 16:20:40 +0100

    updates analysis page routing

--- app.py
+++ app.py
@@ -20,26 +20,28 @@ audio_files = [{'file': "./audio/bonjour.wav",
 
 
 # TODO automate routing
-# Initial Path
+# Home page - displays sound file options for transcribing
 @app.route('/', methods=['GET'])
 def home():
     return render_template("index.html")
 
 
+# Transcription page - displays transcription of selected sound file
 @app.route('/transcribe/<int:file>', methods=['GET'])
 def transcription(file):
     model = whisper.load_model("tiny", device="cpu")
-    text = whisper.transcribe(model=model, audio=audio_files[file]['file'], language=audio_files[file]['language'])
-    return render_template("transcribe.html", text=text['text'])
+    text = whisper.transcribe(model=model, audio=str(audio_files[file]['file']), language=audio_files[file]['language'])
+    # return render_template("transcribe.html", text=text['text'])
+    return render_template("transcribe.html", text=text['text'], file=file), text
 
 
+# Analysis page - displays sentiment analysis of transcription
 @app.route('/transcribe/<int:file>/analyze', methods=['GET'])
-def analysis(file):
-    text = 'text'
+def analysis(file, text):
     # transcript_list = text.split(".")
     # sentiment_pipeline = pipeline("sentiment-analysis")
     # analysis = sentiment_pipeline(transcript_list)
-    return render_template("analyze.html", analysis=text, file=file)
+    return render_template("analyze.html", analysis=text, file=audio_files[file]['file'][8:])
 
 
 # TODO split into two routes, one for each ai
